861d007a223977ce376c778a67eb6a9a529d75cb
Angela Mutava <angela.mutava@andela.com>
2020-02-27 08:08:47 +0300

    setting up login/create account

---
+++ app/application/auth/model.py
@@ -0,0 +1,5 @@
+users = [
+    {"id": 0, "email": "example@gmail.com", "password": "password"},
+    {"id": 1, "email": "example2@gmail.com", "password": "password"},
+    {"id": 2, "email": "example3@gmail.com", "password": "password"},
+]
